d3016acb4197527a09aa8c924a87007d657bd1c4
Marcos <marcos@moriarti.cnio.es>
2023-05-30 15:50:00 +0200

    Add changes for new HER2+ subset-object

--- scripts/reference/2-filter_reference_seuratobj.R
+++ scripts/reference/2-filter_reference_seuratobj.R
@@ -22,71 +22,73 @@ customVln <- function(x, features) {
 }
 
 #Load RDS
-seuratobj.reference <- readRDS(file = "./results/analysis/seuratobj.reference.rds")
+seuratobj.refHER2 <- readRDS(file = "./results/analysis/seuratobj.refHER2.rds")
+head(seuratobj.refHER2@meta.data)
 
-# Create metadata about %Mitochondrial and %Ribosomal
-seuratobj.reference[["percent.mt"]] <- 
-  PercentageFeatureSet(object = seuratobj.reference, pattern = "^MT-")
-seuratobj.reference[["percent.rb"]] <- 
-  PercentageFeatureSet(object = seuratobj.reference, pattern = "^RP[SL][[:digit:]]")
-seuratobj.reference@meta.data <- seuratobj.reference@meta.data %>%
-  mutate(percent.mt = if_else(is.na(percent.mt), 0, percent.mt),
-         percent.rb = if_else(is.na(percent.rb), 0, percent.rb))
-head(seuratobj.reference@meta.data)
+# Create metadata %Ribosomal (mitochondrial is already made)
+seuratobj.refHER2[["Percent_ribo"]] <- 
+  PercentageFeatureSet(object = seuratobj.refHER2, pattern = "^RP[SL][[:digit:]]")
+seuratobj.refHER2@meta.data <- seuratobj.refHER2@meta.data %>%
+  mutate(percent.rb = if_else(is.na(percent.rb), 0, percent.rb))
+head(seuratobj.refHER2@meta.data)
 
 # ViolinPlots before spot filtering
-vln.mt.before <- customVln(seuratobj.reference, features = "percent.mt") +
+Idents(seuratobj.refHER2) <- "celltype_major"
+vln.mt.before <- customVln(seuratobj.refHER2, features = "Percent_mito") +
   ggtitle("Mit Pre-filtering")
-vln.rb.before <- customVln(seuratobj.reference, features = "percent.rb") +
+vln.rb.before <- customVln(seuratobj.refHER2, features = "Percent_ribo") +
   ggtitle("Rib Pre-filtering") 
-vln.count.before <- customVln(seuratobj, features = "nCount_Spatial") +
+vln.count.before <- customVln(seuratobj.refHER2, features = "nCount_RNA") +
   ggtitle("Counts Pre-filtering")
-vln.feat.before <- customVln(seuratobj, features = "nFeature_Spatial") +
+vln.feat.before <- customVln(seuratobj.refHER2, features = "nFeature_RNA") +
   ggtitle("Features Pre-filtering")
 
 pre.violin <- (vln.mt.before | vln.rb.before) / (vln.count.before | vln.feat.before) 
 pre.violin <- pre.violin & theme(legend.position = "bottom") 
 pre.violin <- pre.violin + plot_layout(guides = "collect")
-pre.violin
+pre.violin + 
+  plot_annotation(
+    title = "Prefiltered QC parameters",
+    theme = theme(plot.title = element_text(size = 18, face = "bold"))
+    )
 
-# SpatialFeaturePlots before spot filtering
-sf.mt.before <- SpatialFeaturePlot(seuratobj, features = "percent.mt")
-sf.rb.before <- SpatialFeaturePlot(seuratobj, features = "percent.rb")
-sf.count.before <- SpatialFeaturePlot(seuratobj, features = "nCount_Spatial")
-sf.feat.before <- SpatialFeaturePlot(seuratobj, features = "nFeature_Spatial")
 
-pre.spatial <- (sf.mt.before | sf.rb.before) / (sf.count.before | sf.feat.before)
-
-#vln.mt.before + inset_element(sf.mt.before, left = 0.6, bottom = 0.6, right = 1, top = 1)
-
+# params:
+#   qc:
+#   mt_limits: [0, 15]
+# rb_limits: [0, 40]
+# count_limits: [250, 50000]
+# feat_limits: [200, 7500]
 # Filtering seuratobject: mitochondrial percentaje (<10-15%), nCount (always
 # >0, depends on status 200-500) and nFeature(always >0, depends on status
 # 100-200)
-seuratobj.filtered <- subset(seuratobj, subset = percent.mt < 15 & nCount_Spatial > 500 & nFeature_Spatial > 200)
-dim(seuratobj)
-dim(seuratobj.filtered)
+seuratobj.refHER2.filtered <- subset(seuratobj.refHER2, 
+                                 subset = (Percent_mito < 15) &
+                                   (Percent_ribo < 40) & 
+                                   (nCount_RNA > 250 & nCount_RNA < 50000) &  
+                                   (nFeature_RNA > 200 & nFeature_RNA <7500))
+dim(seuratobj.refHER2)
+dim(seuratobj.refHER2.filtered)
 
 # VlnPlots after spot filtering
-vln.mt.after <- customVln(seuratobj.filtered, features = "percent.mt") + 
+Idents(seuratobj.refHER2.filtered) <- "celltype_major"
+vln.mt.after <- customVln(seuratobj.refHER2.filtered, features = "Percent_mito") + 
   ggtitle("Mit Filtered")
-vln.rb.after <- customVln(seuratobj.filtered, features = "percent.rb") +
+vln.rb.after <- customVln(seuratobj.refHER2.filtered, features = "Percent_ribo") +
   ggtitle("Rb Filtered")
-vln.count.after <- customVln(seuratobj.filtered, features = "nCount_Spatial") +
+vln.count.after <- customVln(seuratobj.refHER2.filtered, features = "nCount_RNA") +
   ggtitle("Count Filtered")
-vln.feat.after <- customVln(seuratobj.filtered, features = "nFeature_Spatial") +
+vln.feat.after <- customVln(seuratobj.refHER2.filtered, features = "nFeature_RNA") +
   ggtitle("Feat Filtered")
 
 post.violin <- (vln.mt.after | vln.rb.after ) / (vln.count.after | vln.feat.after) & theme(legend.position = "bottom")
 post.violin <- post.violin + plot_layout(guides = "collect")
-
-# SpatialFeaturePlots after spot filtering
-sf.mt.after <- SpatialFeaturePlot(seuratobj.filtered, features = "percent.mt")
-sf.rb.after <- SpatialFeaturePlot(seuratobj.filtered, features = "percent.rb")
-sf.count.after <- SpatialFeaturePlot(seuratobj.filtered, features = "nCount_Spatial")
-sf.feat.after <- SpatialFeaturePlot(seuratobj.filtered, features = "nFeature_Spatial")
-
-post.spatial <- (sf.mt.after | sf.rb.after ) / (sf.count.after | sf.feat.after)
+post.violin + 
+  plot_annotation(
+    title = "Postfiltered QC parameters",
+    theme = theme(plot.title = element_text(size = 18, face = "bold"))
+  )
 
 # Save data
 dir.create(path = paste0(out.dir,"/analysis"), recursive = TRUE)
-saveRDS(seuratobj.filtered, file = "./results/analysis/seuratobj.filtered.rds")
+saveRDS(seuratobj.refHER2.filtered, file = "./results/analysis/seuratobj.refHER2.filtered.rds")
