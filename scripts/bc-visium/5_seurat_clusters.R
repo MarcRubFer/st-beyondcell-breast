b6018867f736a3b8991f4ee7517c62a9f95364fc
Marcos <marcos@moriarti.cnio.es>
2023-07-06 15:13:35 +0200

    Changes in kparam and res

--- scripts/bc-visium/5_seurat_clusters.R
+++ scripts/bc-visium/5_seurat_clusters.R
@@ -69,7 +69,7 @@ clustersc3.kparam <- ggplot(data=l, aes(x=SCT_snn_res., y=median.stability, grou
 seuratobj.clusters <- FindNeighbors(seuratobj.phase, 
                                     reduction = "pca", 
                                     dims = 1:20, 
-                                    k.param = 50)
+                                    k.param = 30)
 res <- c(0.1,0.2,0.3,0.4,0.5)
 seuratobj.clusters <- FindClusters(seuratobj.clusters, 
                                    resolution = res, 
@@ -111,7 +111,7 @@ clustree.analysis <- (clustree.plot | max.stability.plot)
 
 cell.types <- names(seuratobj.clusters@meta.data)[6:14]
 df.celltypes.clusters <- seuratobj.clusters@meta.data[6:14]
-df.celltypes.clusters$cluster <- seuratobj.clusters@meta.data$SCT_snn_res.0.2
+df.celltypes.clusters$cluster <- seuratobj.clusters@meta.data$SCT_snn_res.0.3
 df.celltypes.clusters$cluster <- as.factor(df.celltypes.clusters$cluster)
 
 df.celltypes.clusters <- as.data.frame(df.celltypes.clusters)
@@ -127,8 +127,8 @@ boxplot.celltypes.clusters <- ggplot(df.celltypes.clusters.pivot, aes(x = cell.t
   theme(axis.text.x = element_blank(),
         axis.ticks = element_blank()) 
 
-spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.2")
-dim.clusters <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.2")
+spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.3")
+dim.clusters <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.3")
 boxplot.celltypes.clusters / (spatial.clusters | dim.clusters)
 
 breastcancermarkers.spatial <- SpatialFeaturePlot(seuratobj.clusters, features = c("ESR1", "PGR", "ERBB2"), ncol = 4)
