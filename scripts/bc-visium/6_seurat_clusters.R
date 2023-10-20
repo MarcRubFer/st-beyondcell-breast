283b33492265c18705180bd51ce489766655ce84
Marcos <marcos@moriarti.cnio.es>
2023-10-20 14:53:29 +0200

    Add alternat.cellcycle regression and new function

--- scripts/bc-visium/6_seurat_clusters.R
+++ scripts/bc-visium/6_seurat_clusters.R
@@ -19,50 +19,76 @@ seuratobj.phase.alt <- readRDS(file = "./results/analysis/seuratobj.phase_altern
 elbow <- ElbowPlot(seuratobj.phase, 
                    ndims = 50, 
                    reduction = "pca")
-elbow
+elbow.alt <- ElbowPlot(seuratobj.phase.alt, 
+                       ndims = 50, 
+                       reduction = "pca")
+elbow | elbow.alt
 #ggsave(elbow, filename = paste0(out.dir, "/plots/elbow.pdf")) 
 
 # Selection of kNN at different resolution levels
 
 k.param <- c(10,20,30,40,50)
-l <- lapply(X = k.param, FUN = function(x){
-  res <- c(0.1,0.2,0.3,0.4,0.5)
-  a <- FindNeighbors(seuratobj.phase, 
-                     reduction = "pca", 
-                     dims = 1:20, 
-                     k.param = x)
-  a <- FindClusters(a, 
-                    resolution = res, 
-                    verbose = FALSE)
-  # Run UMAP
-  a <- RunUMAP(a, 
-               reduction = "pca", 
-               dims = 1:20, 
-               n.components = 2)
-  clustree.graph <- clustree(a, 
-                             prefix = "SCT_snn_res.",
-                             prop_filter = 0.1, 
-                             node_colour = "sc3_stability",
-                             return = "graph")
-  max.stability <- clustree.graph %>%
-    activate(nodes) %>%
-    as.data.frame() %>%
-    group_by(SCT_snn_res.) %>%
-    summarise(median.stability = median(sc3_stability),
-              n.clusters = length(unique(cluster))) %>%
-    mutate(k.param = x)
-  return(max.stability)
-})
+res <- c(0.1,0.2,0.3,0.4,0.5)
+# Function to create a data.frame with SC3 stability data
+# for each k.parameters and resolution.
+selection.kparam.res <- function(object,k.param,res) {
+  list.data <- lapply(X = k.param, FUN = function(x){
+    res <- res
+    obj.temp <- FindNeighbors(object, 
+                              reduction = "pca", 
+                              dims = 1:20, 
+                              k.param = x)
+    obj.temp <- FindClusters(obj.temp, 
+                             resolution = res, 
+                             verbose = FALSE)
+    # Run UMAP
+    obj.temp <- RunUMAP(obj.temp, 
+                        reduction = "pca", 
+                        dims = 1:20, 
+                        n.components = 2)
+    clustree.graph <- clustree(obj.temp, 
+                               prefix = "SCT_snn_res.",
+                               prop_filter = 0.1, 
+                               node_colour = "sc3_stability",
+                               return = "graph")
+    max.stability <- clustree.graph %>%
+      activate(nodes) %>%
+      as.data.frame() %>%
+      group_by(SCT_snn_res.) %>%
+      summarise(median.stability = median(sc3_stability),
+                n.clusters = length(unique(cluster))) %>%
+      mutate(k.param = x)
+    return(max.stability)
+  })
+  
+  df.data <- list.data %>%
+    bind_rows() %>%
+    mutate(k.param = as.factor(k.param))
+  return(df.data)
+}
 
-l <- l %>%
-  bind_rows() %>%
-  mutate(k.param = as.factor(k.param))
-
-clustersc3.kparam <- ggplot(data=l, aes(x=SCT_snn_res., y=median.stability, group = k.param, color = k.param)) + 
+sc3.phase.data <- selection.kparam.res(object = seuratobj.phase, k.param = k.param, res = res)
+sc3.phase.graph <- ggplot(data=sc3.phase.data, aes(x=SCT_snn_res., y=median.stability, group = k.param, color = k.param)) + 
   geom_line()+
   geom_point() +
   labs(x = "Resolution",
-       y = "Median of SC3 stability")
+       y = "Median of SC3 stability") +
+  ylim(0.0,0.8) +
+  ggtitle(label = "SC3 stability across resolution and k.params", subtitle = "Cell cycle regression: standard") +
+  theme_light()
+
+sc3.phase.alt.data <- selection.kparam.res(object = seuratobj.phase.alt, k.param = k.param, res = res)
+sc3.phase.alt.graph <- ggplot(data=sc3.phase.alt.data, aes(x=SCT_snn_res., y=median.stability, group = k.param, color = k.param)) + 
+  geom_line()+
+  geom_point() +
+  labs(x = "Resolution",
+       y = "Median of SC3 stability") +
+  ylim(0.0,0.8) +
+  ggtitle(label = "SC3 stability across resolution and k.params", subtitle = "Cell cycle regression: alternative") +
+  theme_light() +
+  theme(plot.subtitle = element_text(colour = "red"))
+
+sc3.phase.graph | sc3.phase.alt.graph
 
 
 # Recompute Clustering at selected K.param
@@ -126,39 +152,48 @@ clustree.plot.alt <- clustree(seuratobj.clusters.alt,
 clustree.analysis <- (clustree.plot | clustree.plot.alt)
 
 # Boxplot by cluster, celltypes proportion
+df.celltypes.clusters <- seuratobj.clusters@meta.data %>%
+  select(B.cells:T.cells,SCT_snn_res.0.2) %>%
+  rename(clusters = SCT_snn_res.0.2) %>%
+  mutate(clusters = as.factor(clusters)) %>%
+  pivot_longer(cols = -clusters,
+               names_to = "cell.type",
+               values_to = "cell.prop")
 
-cell.types <- names(seuratobj.clusters@meta.data)[6:14]
-df.celltypes.clusters <- seuratobj.clusters@meta.data[6:14]
-df.celltypes.clusters$cluster <- seuratobj.clusters@meta.data$SCT_snn_res.0.2
-#df.celltypes.clusters$cluster <- seuratobj.clusters@meta.data$SCT_snn_res.0.1
-df.celltypes.clusters$cluster <- as.factor(df.celltypes.clusters$cluster)
-
-df.celltypes.clusters <- as.data.frame(df.celltypes.clusters)
-df.celltypes.clusters.pivot <- pivot_longer(df.celltypes.clusters, 
-                                             cols = -cluster, 
-                                             names_to = "cell.type", 
-                                             values_to = "cell.prop")
-
-boxplot.celltypes.clusters <- ggplot(df.celltypes.clusters.pivot, aes(x = cell.type, y = cell.prop)) +
+boxplot.celltypes.clusters <- ggplot(df.celltypes.clusters, aes(x = cell.type, y = cell.prop)) +
   geom_boxplot(aes(fill = cell.type)) + 
-  facet_wrap(~cluster) +
-  ggtitle(label = "Cell types proportions within each cluster") +
+  facet_wrap(~clusters) +
+  ggtitle(label = "Cell types proportions within each cluster", subtitle = "Cell cycle regression: standard") +
   theme(axis.text.x = element_blank(),
         axis.ticks = element_blank()) 
 
 spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.2")
 dim.clusters <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.2")
 
-#spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.1")
-#dim.clusters <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.1")
-((spatial.clusters / dim.clusters) | boxplot.celltypes.clusters)  + 
-  plot_annotation(tag_levels = 'A')
+df.celltypes.clusters.alt <- seuratobj.clusters.alt@meta.data %>%
+  select(B.cells:T.cells,SCT_snn_res.0.2) %>%
+  rename(clusters = SCT_snn_res.0.2) %>%
+  mutate(clusters = as.factor(clusters)) %>%
+  pivot_longer(cols = -clusters,
+               names_to = "cell.type",
+               values_to = "cell.prop")
 
-breastcancermarkers.spatial <- SpatialFeaturePlot(seuratobj.clusters, features = c("ESR1", "PGR", "ERBB2"), ncol = 4)
-breastcancermarkers.spatial / (spatial.clusters | dim.clusters) 
+boxplot.celltypes.clusters.alt <- ggplot(df.celltypes.clusters.alt, aes(x = cell.type, y = cell.prop)) +
+  geom_boxplot(aes(fill = cell.type)) + 
+  facet_wrap(~clusters) +
+  ggtitle(label = "Cell types proportions within each cluster", subtitle = "Cell cycle regression: alternative") +
+  theme(axis.text.x = element_blank(),
+        axis.ticks = element_blank(), 
+        plot.subtitle = element_text(colour = "red")) 
 
-DimPlot(seuratobj.clusters, group.by = "orig.ident")
+spatial.clusters.alt <- SpatialDimPlot(seuratobj.clusters.alt, group.by = "SCT_snn_res.0.2")
+dim.clusters.alt <- DimPlot(seuratobj.clusters.alt, group.by = "SCT_snn_res.0.2")
 
+
+
+boxplot.celltypes.clusters | boxplot.celltypes.clusters.alt
+spatial.clusters / spatial.clusters.alt
+dim.clusters | dim.clusters.alt
 
 # Save plots and ggplots
 dir.create(path = paste0(out.dir,"/plots/clustering/"), recursive = TRUE)
