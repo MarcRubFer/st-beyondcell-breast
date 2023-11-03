fcdca7821eeb3eaa42da09fb13f27ac6e38b882b
Marcos <marcos@moriarti.cnio.es>
2023-11-03 17:30:47 +0100

    Format for possible final script

--- scripts/bc-visium/6.1_seurat_clusters_new.R
+++ scripts/bc-visium/6.1_seurat_clusters_new.R
@@ -11,9 +11,13 @@ library("tidygraph")
 library("ggsankey")
 #devtools::install_github("igrabski/sc-SHC")
 library("scSHC")
+library("cowplot")
 
 out.dir <- "./results"
 dir.create(path = out.dir, recursive = TRUE)
+
+# Set seed
+set.seed(1)
 
 # Load RDS
 seuratobj.phase <- readRDS(file = "./results/analysis/seuratobj.phase.rds")
@@ -26,7 +30,24 @@ elbow <- ElbowPlot(seuratobj.phase,
 elbow
 #ggsave(elbow, filename = paste0(out.dir, "/plots/elbow.pdf")) 
 
-#Find Neighboors and FindClusters at different resolutions
+# Create SNN clustering graph using Seurat: (https://www.biostars.org/p/9572463/)
+  # Find Neighboors: is a function that is used to find the nearest neighbors of your 
+  #                   single cell data point within a dataset. It works by calculating the neighborhood 
+  #                   overlap (Jaccard index) between every cell and its k. param nearest neighbors. 
+  #                   It's often employed in various applications such as anomaly detection, and 
+  #                   dimensionality reduction. 
+  #                   The concept is that given a data point, you want to identify the closest data points 
+  #                   to it based on some similarity metric, such as Euclidean distance or cosine similarity. 
+  #                   This helps to identify similar points in the dataset, which can be useful for making 
+  #                   predictions or understanding the distribution of the data.
+  
+  # FindClusters: is a function used for clustering data points into groups or 
+  #               clusters based on their similarity. It uses a graph-based clustering approach 
+  #               and a Louvain algorithm. Clustering is an unsupervised learning technique where 
+  #               the algorithm groups similar cells together without any predefined labels. 
+  #               The goal is to find patterns and structure in your data. 
+  #               The number of clusters and the algorithm used can vary based on the problem and data characteristics
+
 seuratobj.clusters <- FindNeighbors(seuratobj.phase, 
                                     reduction = "pca", 
                                     dims = 1:20, 
@@ -47,33 +68,16 @@ clustree.plot <- clustree(seuratobj.clusters,
                           prefix = "SCT_snn_res.",
                           node_colour = "sc3_stability") 
 
-clustree.graph <- clustree(seuratobj.clusters, 
-                           prefix = "SCT_snn_res.",
-                           prop_filter = 0.1, 
-                           node_colour = "sc3_stability",
-                           return = "graph")
 
-max.stability <- clustree.graph %>%
-  activate(nodes) %>%
-  as.data.frame() %>%
-  group_by(SCT_snn_res.) %>%
-  summarise(median.stability = median(sc3_stability),
-            n.clusters = length(unique(cluster)))
 
-max.stability.plot <- ggplot(data=max.stability, aes(x=SCT_snn_res., y=median.stability, group = 1)) + 
-  geom_line()+
-  geom_point() +
-  labs(x = "Resolution",
-       y = "Median of SC3 stability")
 
-max.stability.plot | clustree.plot
 # Test significance of clusters (sc-SHC package)
 # Round scaled data
 scaled <- round(seuratobj.clusters@assays$SCT@data * 100, 0)
 
 # Compute Therapeutic Clusters (TCs)
 # We test significance at every resolution level previous selected. This parameter
-# could be change to test some particular resolutions.
+# could be change to test some particular resolutions. (take some minutes)
 list.scSHC <- lapply(X = res, FUN = function(x){
   original.clusters <- seuratobj.clusters@meta.data %>%
     pull(all_of(paste0("SCT_snn_res.",x))) %>%
@@ -89,130 +93,149 @@ list.scSHC <- lapply(X = res, FUN = function(x){
   return(as.data.frame(new.clusters[[1]]))
   
 })
-ECs <- do.call(cbind, list.scSHC)  
-head(ECs)
-colnames(ECs) <- paste0("EC_scSHC_res.",res)
-rownames(ECs) <- colnames(scaled)
-ECs <- ECs %>%
-  mutate_all(.funs = ~str_replace_all(., "new", "EC-scSHC-")) %>%
+scSHCs <- do.call(cbind, list.scSHC)  
+head(scSHCs)
+colnames(scSHCs) <- paste0("scSHC_res.",res)
+rownames(scSHCs) <- colnames(scaled)
+scSHCs <- scSHCs %>%
+  mutate_all(.funs = ~str_replace_all(., "new", "scSHC-")) %>%
   mutate_all(.funs = ~as.factor(.))
 
+# Add scSHCs data to metadata seurat object
+seuratobj.clusters <- AddMetaData(seuratobj.clusters, metadata = scSHCs)
+head(seuratobj.clusters@meta.data)
+
+#Plot of statistic significance ggsankey SCT vs scSHC
+sankey.scSHC <- seuratobj.clusters@meta.data %>%
+  select(SCT_snn_res.0.2, scSHC_res.0.2) %>%
+  make_long(SCT_snn_res.0.2, scSHC_res.0.2) %>%
+  ggplot(., aes(x = x
+                , next_x = next_x
+                , node = node
+                , next_node = next_node
+                , fill = factor(node)
+                , label = node)) + 
+  geom_sankey(flow.alpha = 0.5
+                      #,flow.fill = "grey"
+                      ,node.color = "black"
+                      ,node.fill = "#aab756"
+                      ,show.legend = T) + 
+  geom_sankey_label(size = 3, color = "black", fill= "white", hjust = 0.5) + 
+  scale_x_discrete(position = "top") + 
+  theme_sankey() +
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(face = "bold"))
+sankey.scSHC
+
+# Create patch of stability and significance clustering
+clustree.sankey <- (clustree.plot | sankey.scSHC) + plot_annotation(tag_levels = "A")
+
+# Once a resolution has been chosen. We create a new variable with 
+# final denomination of expression clusters (ECs)
+head(seuratobj.clusters@meta.data)
+
+ECs <- seuratobj.clusters@meta.data %>%
+  select(scSHC_res.0.2) %>%
+  mutate(scSHC_res.0.2 = str_replace(scSHC_res.0.2,pattern = "scSHC-",replacement = ""),
+         ECs_res.0.2 = case_when(scSHC_res.0.2 == 5 ~ "EC-1",
+                                 scSHC_res.0.2 == 4 ~ "EC-2",
+                                 scSHC_res.0.2 == 8 ~ "EC-3",
+                                 scSHC_res.0.2 == 9 ~ "EC-4",
+                                 scSHC_res.0.2 == 7 ~ "EC-5",
+                                 scSHC_res.0.2 == 6 ~ "EC-6",
+                                 scSHC_res.0.2 == 2 ~ "EC-7",
+                                 scSHC_res.0.2 == 3 ~ "EC-8",
+                                 scSHC_res.0.2 == 1 ~ "EC-9",
+                                 TRUE ~ scSHC_res.0.2),
+         scSHC_res.0.2 = NULL)
+head(ECs)
+
+# Add new variable to metadata
 seuratobj.clusters <- AddMetaData(seuratobj.clusters, metadata = ECs)
 head(seuratobj.clusters@meta.data)
 
-# Collapse all non-tumour cell types in TME category. Add metadata
-dual.spot <- seuratobj.clusters@meta.data %>%
-  select(spot.collapse) %>%
-  mutate(spot.dual = case_when(spot.collapse == "TUMOUR" ~ spot.collapse,
-                               TRUE ~ "TME"))
-head(dual.spot)
-seuratobj.clusters <- AddMetaData(seuratobj.clusters, metadata = dual.spot)
-head(seuratobj.clusters@meta.data)
-#Plot of statistic significance ggsankey SCT vs scSHC
-list.plots.scSHC <- lapply(res, function(x){
-  cluster <- paste0("SCT_snn_res.",x)
-  scSHC <- paste0("EC_scSHC_res.",x)
-  df <- seuratobj.clusters@meta.data %>%
-    select(cluster, scSHC,spot.dual)
-  df <- df %>%
-    make_long(cluster, scSHC,spot.dual)
-  pl <- ggplot(df, aes(x = x
-                       , next_x = next_x
-                       , node = node
-                       , next_node = next_node
-                       , fill = factor(node)
-                       , label = node)
-  )
-  pl <- pl +geom_sankey(flow.alpha = 0.5
-                        ,flow.fill = "grey"
-                        ,node.color = "black"
-                        ,node.fill = "#aab756"
-                        ,show.legend = T)
-  pl <- pl +geom_sankey_label(size = 3, color = "black", fill= "white", hjust = 1)
-  pl <- pl + theme_sankey() +
-    theme(legend.position = "none",
-          axis.title.x = element_blank(),
-          axis.text.x = element_blank())
-  return(pl)
-})
+# UMAP_Dimplot and Spatial Dimplots of ECs.
+# Create colors for expression clusters
+ECs.colors <- c("EC-1" = "#ff944c",
+                "EC-2" = "#76b74b",
+                "EC-3" = "#8762c9",
+                "EC-4" = "#f65b9a",
+                "EC-5" = "#6d8cce",
+                "EC-6" = "#b76512", 
+                "EC-7" = "#4db497",
+                "EC-8" = "#c26083",
+                "EC-9" = "#6c823c")
 
-wrap_plots(list.plots.scSHC)
+dim.clusters <- DimPlot(seuratobj.clusters, group.by = "ECs_res.0.2", cols = ECs.colors) +
+  ggtitle(label = NULL)
+
+
+
+
+spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "ECs_res.0.2", combine = F) 
+spatial.clusters <- lapply(spatial.clusters, function(i){
+  i + scale_fill_manual(values = ECs.colors) + labs(fill = "ECs")
+})
+spatial.clusters <- wrap_plots(spatial.clusters)
+
+
 
 # Boxplot by cluster, celltypes proportion
-df.celltypes.clusters <- seuratobj.clusters@meta.data %>%
-  select(B.cells:T.cells,SCT_snn_res.0.3) %>%
-  rename(clusters = SCT_snn_res.0.3) %>%
-  mutate(clusters = as.factor(clusters)) %>%
-  pivot_longer(cols = -clusters,
-               names_to = "cell.type",
-               values_to = "cell.prop")
+# boxplot.celltypes.clusters <- seuratobj.clusters@meta.data %>%
+#   select(B.cells:T.cells,SCT_snn_res.0.3) %>%
+#   rename(clusters = SCT_snn_res.0.3) %>%
+#   mutate(clusters = as.factor(clusters)) %>%
+#   pivot_longer(cols = -clusters,
+#                names_to = "cell.type",
+#                values_to = "cell.prop") %>%
+#   ggplot(., aes(x = cell.type, y = cell.prop)) +
+#   geom_boxplot(aes(fill = cell.type)) + 
+#   facet_wrap(~clusters) +
+#   theme(axis.text.x = element_blank(),
+#         axis.ticks = element_blank())
 
-boxplot.celltypes.clusters <- ggplot(df.celltypes.clusters, aes(x = cell.type, y = cell.prop)) +
-  geom_boxplot(aes(fill = cell.type)) + 
-  facet_wrap(~clusters) +
-  #ggtitle(label = "Cell types proportions within each cluster", subtitle = "Cell cycle regression: standard") +
-  theme(axis.text.x = element_blank(),
-        axis.ticks = element_blank()) 
-df <- seuratobj.clusters@meta.data %>%
-  select(spot.collapse,SCT_snn_res.0.2) %>%
-  group_by(SCT_snn_res.0.2, spot.collapse) %>%
-  summarise(n = n(),
-  )
-
-total_counts <- df %>%
-  group_by(SCT_snn_res.0.2) %>%
-  summarise(total = sum(n))
-
-df <- df %>%
-  left_join(total_counts, by = "SCT_snn_res.0.2")
-
-df <- df %>%
-  mutate(relat.prop = n / total)
-
-
-head(df)
-
-colors.categories <- toupper(c(#"#4fafe3",
-  "MYELOID" ="#be7adc", #violet
-  "CAFS" = "#dec36f", #ocre
-  "ENDOTHELIAL" = "#549f42", #green
-  "LYMPHOID" = "#f1703a", #orange
-  "OTHERS" ="#79696B", #grey
-  "TUMOUR" = "#c4534e")) #dark.red
+# Barplot cell types proportion by expr. clusters (ECs)
+colors.categories <- toupper(c("MYELOID" ="#be7adc", #violet
+                               "CAFS" = "#dec36f", #ocre
+                               "ENDOTHELIAL" = "#549f42", #green
+                               "LYMPHOID" = "#f1703a", #orange
+                               "OTHERS" ="#79696B", #grey
+                               "TUMOUR" = "#c4534e")) #dark.red
 
 names(colors.categories)
 colors.categories <- colors.categories[order(names(colors.categories))]
 colors.categories
-barplot.celltypes.clusters <- ggplot(df, aes(x=SCT_snn_res.0.2, y=relat.prop, fill = spot.collapse)) +
+
+cell.types.by.EC <- seuratobj.clusters@meta.data %>%
+  select(spot.collapse,ECs_res.0.2) %>%
+  group_by(ECs_res.0.2, spot.collapse) %>%
+  summarise(n = n())
+
+total_counts <- cell.types.by.EC %>%
+  group_by(ECs_res.0.2) %>%
+  summarise(total = sum(n))
+
+final.df <- cell.types.by.EC %>%
+  left_join(total_counts, by = "ECs_res.0.2")
+
+final.df <- final.df %>%
+  mutate(relat.prop = n / total)
+
+barplot.celltypes.clusters <- ggplot(final.df, aes(x=ECs_res.0.2, y=relat.prop, fill = spot.collapse)) +
   geom_bar(stat = "identity") + 
-  scale_fill_manual(values = colors.categories)
+  scale_fill_manual(values = colors.categories) +
+  ylab(label = "Relative proportion") +
+  labs(fill = "Cell type categories") +
+  theme_minimal() +
+  theme(axis.line.y = element_line(colour = "grey"),
+        axis.line.x = element_line(colour = "grey"),
+        axis.title.y = element_text(vjust = 1.5),
+        axis.title.x = element_blank())
 
 barplot.celltypes.clusters
-spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.1")
-dim.clusters <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.1")
-
-#df.celltypes.clusters.alt <- seuratobj.clusters.alt@meta.data %>%
-#  select(B.cells:T.cells,SCT_snn_res.0.2) %>%
-#  rename(clusters = SCT_snn_res.0.2) %>%
-#  mutate(clusters = as.factor(clusters)) %>%
-#  pivot_longer(cols = -clusters,
-#               names_to = "cell.type",
-#               values_to = "cell.prop")
-#
-#boxplot.celltypes.clusters.alt <- ggplot(df.celltypes.clusters.alt, aes(x = cell.type, y = cell.prop)) +
-#  geom_boxplot(aes(fill = cell.type)) + 
-#  facet_wrap(~clusters) +
-#  ggtitle(label = "Cell types proportions within each cluster", subtitle = "Cell cycle regression: alternative") +
-#  theme(axis.text.x = element_blank(),
-#        axis.ticks = element_blank(), 
-#        plot.subtitle = element_text(colour = "red")) 
-#
-#spatial.clusters.alt <- SpatialDimPlot(seuratobj.clusters.alt, group.by = "SCT_snn_res.0.3")
-#dim.clusters.alt <- DimPlot(seuratobj.clusters.alt, group.by = "SCT_snn_res.0.2")
 
 
-dim.clusters | (boxplot.celltypes.clusters / spatial.clusters)
-DimPlot(seuratobj.clusters, group.by = "spot.collapse")
 
 # Collapse all non-tumour cell types in TME category. Add metadata
 dual.spot <- seuratobj.clusters@meta.data %>%
@@ -226,74 +249,68 @@ seuratobj.clusters <- AddMetaData(seuratobj.clusters, metadata = dual.spot)
 dual.colors <- c(
   "TUMOUR" = "#c4534e",
   "TME" = "#098cf0")
-dim.dual <- DimPlot(seuratobj.clusters, group.by = "spot.dual", cols = dual.colors)
-spatial.dual <- SpatialDimPlot(seuratobj.clusters, group.by = "spot.dual", cols = dual.colors)
+dim.dual <- DimPlot(seuratobj.clusters, group.by = "spot.dual", cols = dual.colors) +
+  ggtitle(label = NULL)
 
-dim.clusters <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.1")
-dim.clusters.2 <- DimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.2")
-
-spatial.clusters <- SpatialDimPlot(seuratobj.clusters, group.by = "SCT_snn_res.0.1",combine = F)
-spatial.clusters <- lapply(seq_along(spatial.clusters), FUN = function(i){
-  spatial.clusters[[i]] +
-    scale_fill_manual(values = ECs.colors)
+spatial.dual <- SpatialDimPlot(seuratobj.clusters, group.by = "spot.dual", cols = dual.colors, combine = F)
+spatial.dual <- lapply(spatial.dual, function(i) {
+  i + labs(fill = "Tumour-TME")
 })
-wrap_plots(spatial.clusters)
-spatial.clusters.2 <- SpatialDimPlot(seuratobj.clusters, group.by = "EC_scSHC_res.0.2", combine = F) 
-spatial.clusters.2 <- lapply(seq_along(spatial.clusters.2), FUN = function(x){
-  spatial.clusters.2[[x]] +
-    scale_fill_manual(values = ECs.colors)
-})
-wrap_plots(spatial.clusters)/wrap_plots(spatial.clusters.2)
-ECs.colors <- c("#ce57ad",
-                "#76b74b",
-                "#8762c9",
-                "#c59442",
-                "#6d8cce",
-                "#cb5842",
-                "#4db497",
-                "#c26083",
-                "#6c823c")
+spatial.dual <- wrap_plots(spatial.dual)
 
-panel.dimplots <- (dim.dual | dim.clusters.2) & ggtitle(label = NULL)
-left.panels <- ((panel.dimplots / plot_spacer() / barplot.celltypes.clusters) + plot_layout(heights = c(1,0.1,1.5)))
-right.panels <- ((spatial.dual[[1]] / spatial.dual[[2]]) | (spatial.clusters.2[[1]] / spatial.clusters.2[[2]])) 
-(left.panels | right.panels) + plot_annotation(tag_levels = "A")
+# Create a final figure for clustering
+panel.a <- dim.clusters + 
+  theme(legend.text = element_text(size = 8))
+panel.b <- dim.dual +
+  theme(legend.text = element_text(size = 8))
+panel.c <- barplot.celltypes.clusters
+panel.d <- spatial.dual + plot_layout(guides = "collect")
+panel.e <- spatial.clusters + plot_layout(guides = "collect")
 
-(clustree.plot / list.plots.scSHC[[2]]) | left.panels | right.panels
+dim.panel <- plot_grid(panel.a,panel.b, align = "h", labels = c("A","B"))
+left.panels <- plot_grid(dim.panel,panel.c, ncol = 1, labels = c("","C"), rel_heights = c(1,1.5))
+right.panel <- plot_grid(panel.d,panel.e, labels = c("D","E"), ncol = 1)
 
-((clustree.plot / list.plots.scSHC[[2]]) + plot_layout(heights = c(2,1))) |  ((spatial.dual)/wrap_plots(spatial.clusters.2)/barplot.celltypes.clusters)
+figure <- left.panels | right.panel
 
 
 # Save plots and ggplots
-dir.create(path = paste0(out.dir,"/plots/clustering/"), recursive = TRUE)
-ggsave(filename = "SC3stability_by_kparams.png",
-       plot = clustersc3.kparam,
-       path = paste0(out.dir,"/plots/clustering/"))
-ggsave(filename = "clustree_analysis.png",
-       plot = clustree.analysis,
-       path = paste0(out.dir,"/plots/clustering/"))
-ggsave(filename = "boxplot_celltypes_by_clusters.png",
-       plot = boxplot.celltypes.clusters,
-       path = paste0(out.dir,"/plots/clustering/"))
-ggsave(filename = "clusters_on_slides.png",
-       plot = spatial.clusters,
-       path = paste0(out.dir,"/plots/clustering/"))
-ggsave(filename = "dimplot_clusters.png",
+ggsave(filename = "clustree.png",
+       plot = clustree.plot,
+       path = "./results/plots/clustering/")
+ggsave(filename = "sankeydiagram_res_0.2.png",
+       plot = sankey.scSHC,
+       path = "./results/plots/clustering/")
+ggsave(filename = "clustree_sankey.png",
+       plot = clustree.sankey,
+       path = "./results/plots/clustering/")
+ggsave(filename = "dimplot_clusters_res_0.2.png",
        plot = dim.clusters,
-       path = paste0(out.dir,"/plots/clustering/"))
-ggsave(filename = "spatial_clusters.png",
+       path = "./results/plots/clustering/")
+ggsave(filename = "spatialdimplot_clusters_res.0.2.png",
        plot = spatial.clusters,
-       path = paste0(out.dir,"/plots/clustering/"))
-ggsave(filename = "breastcancermarkers_on_slides.png",
-       plot = breastcancermarkers.spatial,
-       path = paste0(out.dir,"/plots/clustering/"))
+       path = "./results/plots/clustering/")
+ggsave(filename = "barplot_clusters_celltypes.png",
+       plot = barplot.celltypes.clusters,
+       path = "./results/plots/clustering/")
+ggsave(filename = "dimplot_Tumor_TME.png",
+       plot = dim.dual,
+       path = "./results/plots/clustering/")
+ggsave(filename = "spatialdimplot_tumor_tme.png",
+       plot = spatial.dual,
+       path = "./results/plots/clustering/")
+ggsave(filename = "figure_clustering.png",
+       plot = figure,
+       path = "./results/plots/clustering/")
 
-all.plots <- list(clustersc3.kparam, clustree.analysis, boxplot.celltypes.clusters,spatial.clusters, dim.clusters, breastcancermarkers.spatial)
+all.plots <- list(clustree.plot,sankey.scSHC,clustree.sankey,dim.clusters,
+                  spatial.clusters,barplot.celltypes.clusters,dim.dual,
+                  spatial.dual,figure)
 save(all.plots, file = paste0("./results/ggplots/clustering.RData"))
 
 # Save data
 saveRDS(seuratobj.clusters, file = "./results/analysis/seuratobj.clusters.rds")
-saveRDS(seuratobj.clusters.alt, file = "./results/analysis/seuratobj.clusters.alt.rds")
+
 
 
 
