62149fb0c910d83a2a8e5255ed8a886d0a54c75a
Marcos <marcos@moriarti.cnio.es>
2023-11-16 17:45:02 +0100

    Add code GESECA and DEA TME-TCs

--- scripts/bc-visium/GESECA_script.R
+++ scripts/bc-visium/GESECA_script.R
@@ -1,40 +1,40 @@
 reactome.gmt <- readGMT(x= "./data/gmts/reactome_functional.gmt")
+hallmarks.gmt <- readGMT(x="./data/gmts/hallmarks.all.v2023.1.Hs.symbols.gmt")
+
 
 obj <- seuratobj.tcs
-obj <- SCTransform(obj, assay = "Spatial", verbose = T, variable.features.n = 10000)
+obj <- SCTransform(obj, assay = "Spatial", new.assay.name = "SCT_GESECA", verbose = T, variable.features.n = 10000)
 
-obj <- RunPCA(obj, assay = "SCT", verbose = FALSE,
+obj <- RunPCA(obj, assay = "SCT_GESECA", verbose = FALSE,
               rev.pca = TRUE, reduction.name = "pca.rev",
               reduction.key="PCR_", npcs = 50)
-E <- obj@reductions$pca.rev@feature.loadings
+pca.rev <- obj@reductions$pca.rev@feature.loadings
 set.seed(1)
 reactome.gmt
 
 #pctVar -- percent of explained variance along gene set;
-gesecaRes <- geseca(reactome.gmt, E, minSize = 15, maxSize = 500, center = FALSE)
+gesecaRes <- geseca(reactome.gmt, pca.rev, minSize = 15, maxSize = 500, center = FALSE)
 head(gesecaRes, 10)
 topPathways <- gesecaRes[, pathway] |> head(50)
-titles <- sub("HALLMARK_", "", topPathways)
-ps <- plotCoregulationProfileSpatial(reactome.gmt[topPathways], 
-                                     object = obj,
-                                     title=titles)
-cowplot::plot_grid(plotlist=ps, ncol=3)
 
+geseca.hallmarks <- geseca(hallmarks.gmt, pca.rev, minSize = 15, maxSize = 500, center = FALSE)
+head(geseca.hallmarks, 10)
+topPathways.hallmarks <- geseca.hallmarks[, pathway] |> head(50)
 
 x <- GetAssay(obj, assay=DefaultAssay(obj))
 E <- x@scale.data
 prefix=""
 
 res <- obj
-pathway <- reactome.gmt[[5]]
-pathway <- intersect(unique(pathway), rownames(E))
-
-split <- E[pathway, 1:5, drop=FALSE]
-colSums(split)
-score <- colSums(E[pathway, , drop=FALSE])/sqrt(length(pathway))
-score <- scale(score, center=TRUE, scale=F)
-
-score.p <- scale(score[1], center=TRUE, scale=F)
+#pathway <- reactome.gmt[[5]]
+#pathway <- intersect(unique(pathway), rownames(E))
+#
+#split <- E[pathway, 1:5, drop=FALSE]
+#colSums(split)
+#score <- colSums(E[pathway, , drop=FALSE])/sqrt(length(pathway))
+#score <- scale(score, center=TRUE, scale=F)
+#
+#score.p <- scale(score[1], center=TRUE, scale=F)
 #res@meta.data[[paste0(prefix, names(reactome.gmt)[2])]] <- score
 #head(res@meta.data)
 pathways = reactome.gmt[topPathways]
@@ -46,7 +46,15 @@ for (i in seq_along(pathways)) {
   score <- scale(score, center=TRUE, scale=T)
   res@meta.data[[paste0(prefix, names(pathways)[i])]] <- score
 }
-
+hallmarks <- hallmarks.gmt[topPathways.hallmarks]
+for (i in seq_along(hallmarks)) {
+  pathway <- hallmarks[[i]]
+  pathway <- intersect(unique(pathway), rownames(E))
+  
+  score <- colSums(E[pathway, , drop=FALSE])/sqrt(length(pathway))
+  score <- scale(score, center=TRUE, scale=T)
+  res@meta.data[[paste0(prefix, names(hallmarks)[i])]] <- score
+}
 analysis <- res@meta.data %>%
   select(TCs_res.0.3, all_of(starts_with("REACTOME")))
 
@@ -58,6 +66,11 @@ head(matrix)
 tmatrix <- t(matrix)
 head(tmatrix)
 
+hallmarks.matrix <- res@meta.data %>%
+  select(all_of(starts_with("HALLMARK")))
+hallmarks.matrix <- t(as.matrix.data.frame(hallmarks.matrix))
+
+rm(res)
 library(ComplexHeatmap)
 
 col.order <- res@meta.data %>%
@@ -69,6 +82,26 @@ col.order.spots <- col.order  %>%
   pull(spots)
 
 tmatrix <- tmatrix[,col.order.spots]
+hallmarks.matrix <- hallmarks.matrix[ ,col.order.spots]
+
+colors.categories <- toupper(c(#"#4fafe3",
+  "MYELOID" ="#be7adc", #violet
+  "CAFS" = "#dec36f", #ocre
+  "ENDOTHELIAL" = "#549f42", #green
+  "LYMPHOID" = "#f1703a", #orange
+  "OTHERS" ="#79696B", #grey
+  "TUMOUR" = "#c4534e")) #dark.red
+names(colors.categories)
+colors.categories <- colors.categories[order(names(colors.categories))]
+colors.categories
+
+TC.colors <- c("TC-1" = "#00b2d7",
+               "TC-2" = "#e5c22f",
+               "TC-3" = "#903ca2",
+               "TC-4" = "#3f8741",
+               "TC-5" = "#ff7b00",
+               "TC-6" = "#cb5c42")
+
 ht <- Heatmap(
   matrix = tmatrix,
   top_annotation = HeatmapAnnotation("TCs" = col.order$TCs_res.0.3,
@@ -84,12 +117,28 @@ ht <- Heatmap(
 )
 ht
 
+ht.hallmarks <- Heatmap(
+  matrix = hallmarks.matrix,
+  top_annotation = HeatmapAnnotation("TCs" = col.order$TCs_res.0.3,
+                                     "Cell type" = col.order$spot.collapse,
+                                     col = list("TCs" = TC.colors,
+                                                "Cell type" = colors.categories)),
+  show_column_names = F,
+  cluster_columns = F,
+  column_split = col.order$TCs_res.0.3,
+  row_names_gp = gpar(fontsize = 6),
+  #row_km = 4,
+  row_split = 5
+)
+ht.hallmarks
+
 plotCoregulationProfileSpatial(reactome.gmt["REACTOME_EXTRACELLULAR_MATRIX_ORGANIZATION"], 
                                object = obj,
                                title=NULL)
-a <- SpatialFeaturePlot(res, features = "REACTOME_CELL_CYCLE")
+tc.spatial <- SpatialDimPlot(res, group.by = "TCs_res.0.3")
+a <- SpatialFeaturePlot(res, features = "REACTOME_COMPLEMENT_CASCADE")
 
-p <- Seurat::SpatialFeaturePlot(res, features = "REACTOME_CELL_CYCLE",
+p <- Seurat::SpatialFeaturePlot(res, features = "REACTOME_COMPLEMENT_CASCADE",
                                 combine = FALSE, image.alpha = 0)[[1]]
 p$scales$scales[p$scales$find("fill")] <- NULL
 
@@ -108,4 +157,9 @@ suppressMessages({
 p2
 p
 
-a[[1]] | p | p2
+a[[1]] | p | p2 | tc.spatial[[1]]
+
+tumour.tc2.spots <- res@meta.data %>%
+  filter(TCs_res.0.3 == "TC-2" & spot.collapse == "TUMOUR") %>%
+  rownames_to_column("spots") %>%
+  pull(spots)
--- scripts/bc-visium/GSEA_TME_TCs.R
+++ scripts/bc-visium/GSEA_TME_TCs.R
@@ -19,7 +19,7 @@ dir.create(path = out.dir, recursive = TRUE)
 # Read SeuratObjects
 seuratobj.tcs <- readRDS(file = "./results/analysis/seuratobj.therapeutic.clusters.rds")
 
-reactome.gmt <- readGMT(x= "./data/gmts/c2.cp.reactome.v2023.1.Hs.symbols.gmt")
+reactome.gmt <- readGMT(x= "./data/gmts/c2.cp.reactome.v2023.2.Hs.symbols.gmt")
 
 # stablish seed
 set.seed(1)
@@ -32,6 +32,11 @@ TME.dea.gsea <- FindMarkers(seuratobj.tcs,
                             min.pct = 0, 
                             logfc.threshold = 0, 
                             test.use = "wilcox")
+
+write.table(x = TME.dea.gsea,
+            file = "./results/tables/TC1_TC2_findMarkers_results.tsv",
+            sep = "\t", 
+            col.names = TRUE, row.names = FALSE, quote = FALSE)
 
 names.vector.fgsea <- TME.dea.gsea %>%
   rownames_to_column("gene") %>%
@@ -48,37 +53,48 @@ fgseaRes <- fgsea(pathways = reactome.gmt,
                   maxSize  = 500,
                   nPermSimple = 1000)
 
+write.table(x = fgseaRes,
+            file = "./results/tables/TC1_TC2_fGSEA_results.tsv",
+            sep = "\t", 
+            col.names = TRUE, row.names = FALSE, quote = FALSE)
+
+fgseaRes <- as.data.frame(fgseaRes)
+
+barplot.data <- fgseaRes %>%
+  filter(padj < 0.05) %>%
+  mutate(UP_DOWN = case_when(NES > 0 ~ "UP",
+                             TRUE ~ "DOWN")) %>%
+  group_by(UP_DOWN) %>%
+  arrange(desc(NES)) %>%
+  slice_max(NES, n = 50) 
+
+barplot.data.export <- barplot.data %>%
+  select(-leadingEdge)
+
+write.table(x = barplot.data.export,
+            file = "./results/tables/TC1_TC2_barplot_data.tsv",
+            sep = "\t", 
+            col.names = TRUE, row.names = FALSE, quote = FALSE)
+
+barplot.data.annotated <- read_tsv(file = "./data/tsv/TC1_TC2_barplot_data_reannotated.tsv")
+
+barplot.data <- barplot.data %>%
+  mutate(REACTOME_METAPROGRAM = barplot.data.annotated$REACTOME_METAPROGRAM)
+
+barplot <- barplot.data %>%
+  ggplot(aes(y = reorder(as.factor(pathway), REACTOME_METAPROGRAM), x = NES)) +
+    geom_bar(aes(fill = REACTOME_METAPROGRAM),stat = "identity") 
+ 
+barplot <- barplot.data %>%
+  ggplot(aes(y = REACTOME_METAPROGRAM)) +
+  geom_bar(aes(fill=REACTOME_METAPROGRAM)) +
+  scale_x_continuous(breaks = c(1:10)) 
+barplot  
+  
+  
 topPathwaysUp <- fgseaRes[ES > 0][head(order(padj), n=20), pathway]
 plotGseaTable(pathways = reactome.gmt[topPathwaysUp], 
               stats = log2fc.vector, 
               fgseaRes = fgseaRes, 
               gseaParam=0.5)
 
-obj@reductions$pca.rev@feature.loadings
-
-E <- seuratobj.tcs@reductions$pca@feature.loadings
-seuratobj.tcs@assays$SCT@data
-
-set.seed(1)
-gesecaRes <- geseca(reactome.gmt, E, minSize = 15, maxSize = 500, center = FALSE)
-head(gesecaRes, 10)
-topPathways <- gesecaRes[, pathway] |> head(4)
-titles <- sub("HALLMARK_", "", topPathways)
-ps <- plotCoregulationProfileSpatial(reactome.gmt[topPathways], 
-                                     seuratobj.tcs,
-                                     title=titles)
-cowplot::plot_grid(plotlist=ps, ncol=2)
-
-plotGesecaTable(gesecaRes = gesecaRes, pathways = reactome.gmt[topPathways], E = E)
-
-
-
-x <- GetAssay(seuratobj.tcs, assay=DefaultAssay(object))
-E <- x@scale.data
-
-res <- object
-pathway <- reactome.gmt[[2]]
-pathway <- intersect(unique(pathway), rownames(E))
-
-score <- colSums(E[pathway, , drop=FALSE])/sqrt(length(pathway))
-score <- scale(score, center=TRUE, scale=T)
