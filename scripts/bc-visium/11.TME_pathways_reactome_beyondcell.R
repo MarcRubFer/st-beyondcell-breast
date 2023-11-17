090d32745468ed2b1161f66393bfff7162e25995
Marcos <marcos@moriarti.cnio.es>
2023-11-17 17:31:07 +0100

    New script analysis TME

---
+++ scripts/bc-visium/11.TME_pathways_reactome_beyondcell.R
@@ -0,0 +1,124 @@
+rm(list = ls())
+
+library("beyondcell")
+library("Seurat")
+library("tidyverse")
+library("tidygraph")
+library("patchwork")
+library("cowplot")
+library("viridis")
+library("RColorBrewer")
+library("fgsea")
+library("ComplexHeatmap")
+library("circlize")
+
+
+# stablish seed
+set.seed(1)
+
+out.dir <- "./results"
+dir.create(path = out.dir, recursive = TRUE)
+
+seuratobj.tcs <- readRDS(file = "./results/analysis/seuratobj.therapeutic.clusters.rds")
+bc.recomputed <- readRDS(file = "./results/analysis/beyondcell_allspots_breastsignature.rds")
+
+reactome.gmt <- readGMT(x= "./data/gmts/c2.cp.reactome.v2023.2.Hs.symbols.gmt")
+fgseaRes.TME <- read_tsv("./results/tables/TC1_TC2_fGSEA_results.tsv")
+
+# Analysis of TME TCs (TCs 1 and 2)
+TCs.TME <- levels(bc.recomputed@meta.data$TCs_res.0.3)[1:2]
+cells.TCs.TME <- bc.recomputed@meta.data %>%
+  filter(TCs_res.0.3 %in% TCs.TME) %>%
+  rownames_to_column(var = "spots") %>%
+  pull(spots)
+
+seurat.TME <- subset(seuratobj.tcs, cells = cells.TCs.TME)
+dim(seurat.TME)
+
+reactome.bc.gmt <- GenerateGenesets(x= "./data/gmts/reactome_bc_modificado.gmt")
+bc.TME.reactome <- bcScore(sc = seurat.TME,
+                           gs = reactome.bc.gmt,
+                           expr.thres = 0.1)
+
+bc.TME.reactome <- bcUMAP(bc.TME.reactome, 
+                        pc = 10, 
+                        k.neighbors = 20, 
+                        res = 0.2)
+# Extract name of top 50 pathways
+top.50 <- fgseaRes %>%
+  filter(padj < 0.05) %>%
+  mutate(UP_DOWN = case_when(NES > 0 ~ "UP",
+                             TRUE ~ "DOWN")) %>%
+  group_by(UP_DOWN) %>%
+  arrange(desc(NES)) %>%
+  slice_max(NES, n = 50) %>%
+  pull(pathway)
+
+#Colors for cell types categories
+colors.categories <- toupper(c(#"#4fafe3",
+  "MYELOID" ="#be7adc", #violet
+  "CAFS" = "#dec36f", #ocre
+  "ENDOTHELIAL" = "#549f42", #green
+  "LYMPHOID" = "#f1703a", #orange
+  "OTHERS" ="#79696B", #grey
+  "TUMOUR" = "#c4534e")) #dark.red
+names(colors.categories)
+colors.categories <- colors.categories[order(names(colors.categories))]
+colors.categories
+
+TC.colors <- c("TC-1" = "#00b2d7",
+               "TC-2" = "#e5c22f",
+               "TC-3" = "#903ca2",
+               "TC-4" = "#3f8741",
+               "TC-5" = "#ff7b00",
+               "TC-6" = "#cb5c42")
+
+# Extract normalized data for top50 pathway
+pathway.matrix <- bc.TME.reactome@normalized[top.50,]
+pathway.matrix <- bc.TME.reactome@scaled[top.50,]
+dim(pathway.matrix)
+
+col.order <- bc.TME.reactome@meta.data %>%
+  select(TCs_res.0.3, spot.collapse) %>%
+  arrange(TCs_res.0.3, spot.collapse)
+
+col.order.spots <- col.order  %>%
+  rownames_to_column("spots") %>%
+  pull(spots)
+
+pathway.matrix <- pathway.matrix[,col.order.spots]
+
+scaled.matrix <- t(scale(t(pathway.matrix)))
+
+## Create heatmap
+heatmap.pathway <- Heatmap(
+  pathway.matrix,
+  #scaled.matrix,
+  name = "bcScore",
+  cluster_columns = FALSE,
+  top_annotation = HeatmapAnnotation("TCs" = col.order$TCs_res.0.3,
+                                     "Cell type" = col.order$spot.collapse,
+                                     #"Cell type" = anno_block(gp = gpar(fill = colors.categories),
+                                     #                         labels = levels(as.factor(col.order$spot.collapse)),
+                                     #                         labels_gp = gpar(fontsize = 5)),
+                                     col = list("TCs" = TC.colors,
+                                                "Cell type" = colors.categories)),
+  #right_annotation = rowAnnotation(MoA = collapsed.moas$collapsed.MoAs,
+  #                                 col = list(MoA = col.moas)),
+  #left_annotation = rowAnnotation(labels = c(1:15)),
+  #row_dend_reorder = TRUE,
+  show_column_names = FALSE,
+  column_split = col.order$TCs_res.0.3,
+  column_title_gp = gpar(fontsize = 6, angle = 90),
+  #column_order = col.order,
+  row_names_gp = gpar(fontsize = 6),
+  #row_labels = toupper(collapsed.moas$preferred.drug.names),
+  #row_km = 6,
+  #row_order = 1:16,
+  row_split = 8,
+  #show_row_dend = F,
+  #row_title = NULL,
+  #col = colorRamp2(c(drugs.min.matrix, 0, drugs.max.matrix), c("blue", "white", "red")),
+  #heatmap_legend_param = list(at = c(drugs.min.matrix, 0, drugs.max.matrix))
+)      
+heatmap.pathway
--- scripts/bc-visium/GSEA_TME_TCs.R
+++ scripts/bc-visium/GSEA_TME_TCs.R
@@ -9,8 +9,7 @@ library("cowplot")
 library("viridis")
 library("RColorBrewer")
 library("fgsea")
-library("ggseabubble")
-library("ggtree")
+
 
 
 out.dir <- "./results"
@@ -24,41 +23,75 @@ reactome.gmt <- readGMT(x= "./data/gmts/c2.cp.reactome.v2023.2.Hs.symbols.gmt")
 # stablish seed
 set.seed(1)
 
+# Change to Spatial assay and establish TCs as idents
 DefaultAssay(seuratobj.tcs) <- "Spatial"
 Idents(seuratobj.tcs) <- "TCs_res.0.3"
-TME.dea.gsea <- FindMarkers(seuratobj.tcs, 
+
+# Check if results were obtained previously
+path.to.results <- "./results/tables/TC1_TC2_findMarkers_results.tsv"
+
+if (file.exists(path.to.results)) {
+  # Load file from path
+  TME.dea.gsea <- read_tsv(file = "./results/tables/TC1_TC2_findMarkers_results.tsv")
+} else {
+  # Obtain differential expressed markers/genes between TC-1 and TC-2
+  TME.dea.gsea <- FindMarkers(seuratobj.tcs, 
                             ident.1 = "TC-1",
                             ident.2 = "TC-2",
                             min.pct = 0, 
                             logfc.threshold = 0, 
                             test.use = "wilcox")
-
-write.table(x = TME.dea.gsea,
+  # Move gene names (rownames) to a new column
+  TME.dea.gsea <- TME.dea.gsea %>%
+    rownames_to_column("gene")
+  
+  # Export results as tsv table
+  write.table(x = TME.dea.gsea,
             file = "./results/tables/TC1_TC2_findMarkers_results.tsv",
             sep = "\t", 
             col.names = TRUE, row.names = FALSE, quote = FALSE)
+  }
 
+# Fast-GSEA: 
+## For fgsea we need a named vector with the selected parameter (avg_log2FC)
+## Extract gene names
 names.vector.fgsea <- TME.dea.gsea %>%
-  rownames_to_column("gene") %>%
   pull(gene)
 
+## Extract avg_log2FC values
 log2fc.vector <- TME.dea.gsea %>%
   select(avg_log2FC) %>%
   pull()
 
+## Naming the vector
 names(log2fc.vector) <- names.vector.fgsea
+
+## Calculate GSEA results
 fgseaRes <- fgsea(pathways = reactome.gmt, 
                   stats    = log2fc.vector,
                   minSize  = 15,
                   maxSize  = 500,
                   nPermSimple = 1000)
 
-write.table(x = fgseaRes,
+## TODO: create a table which include leadingedge list. At the moment, we save 
+## fgseaRes without this column
+
+fgseaRes.toexport <- fgseaRes %>%
+  select(-leadingEdge)
+
+write.table(x = fgseaRes.toexport,
             file = "./results/tables/TC1_TC2_fGSEA_results.tsv",
             sep = "\t", 
             col.names = TRUE, row.names = FALSE, quote = FALSE)
 
+
+fgseaRes <- read_tsv("./results/tables/TC1_TC2_fGSEA_results.tsv")
+
 fgseaRes <- as.data.frame(fgseaRes)
+
+# In this case we have only two TCs, so we cannot use ggbubbleheatmap (multiple 
+# comparations). For this results we decided to use a standard barplot
+
 
 barplot.data <- fgseaRes %>%
   filter(padj < 0.05) %>%
@@ -68,6 +101,11 @@ barplot.data <- fgseaRes %>%
   arrange(desc(NES)) %>%
   slice_max(NES, n = 50) 
 
+barplot <- barplot.data %>%
+  ggplot(aes(y = reorder(as.factor(pathway), NES), x = NES)) +
+  geom_bar(aes(fill = "firebrick2"), stat = "identity") 
+
+
 barplot.data.export <- barplot.data %>%
   select(-leadingEdge)
 
@@ -76,25 +114,35 @@ write.table(x = barplot.data.export,
             sep = "\t", 
             col.names = TRUE, row.names = FALSE, quote = FALSE)
 
+
+
 barplot.data.annotated <- read_tsv(file = "./data/tsv/TC1_TC2_barplot_data_reannotated.tsv")
 
-barplot.data <- barplot.data %>%
+barplot.data2 <- barplot.data %>%
   mutate(REACTOME_METAPROGRAM = barplot.data.annotated$REACTOME_METAPROGRAM)
 
-barplot <- barplot.data %>%
-  ggplot(aes(y = reorder(as.factor(pathway), REACTOME_METAPROGRAM), x = NES)) +
+barplot2 <- barplot.data2 %>%
+  ggplot(aes(y = reorder(as.factor(pathway), NES), x = NES)) +
     geom_bar(aes(fill = REACTOME_METAPROGRAM),stat = "identity") 
  
-barplot <- barplot.data %>%
+barplot3 <- barplot.data2 %>%
   ggplot(aes(y = REACTOME_METAPROGRAM)) +
   geom_bar(aes(fill=REACTOME_METAPROGRAM)) +
   scale_x_continuous(breaks = c(1:10)) 
-barplot  
+barplot3
+
+top10.pathways <- fgseaRes %>%
+  filter(padj < 0.05) %>%
+  mutate(UP_DOWN = case_when(NES > 0 ~ "UP",
+                             TRUE ~ "DOWN")) %>%
+  group_by(UP_DOWN) %>%
+  arrange(desc(NES)) %>%
+  slice_max(NES, n = 10) 
   
-  
-topPathwaysUp <- fgseaRes[ES > 0][head(order(padj), n=20), pathway]
-plotGseaTable(pathways = reactome.gmt[topPathwaysUp], 
-              stats = log2fc.vector, 
-              fgseaRes = fgseaRes, 
-              gseaParam=0.5)
+ribosomal <- seuratobj.tcs@meta.data %>%
+  select(percent.rb,spot.collapse,TCs_res.0.3) %>%
+  filter(TCs_res.0.3 == "TC-1" | TCs_res.0.3 == "TC-2") %>%
+  ggplot(aes(x=TCs_res.0.3, y=percent.rb)) +
+  geom_boxplot(aes(fill = spot.collapse))
+ribosomal
 
